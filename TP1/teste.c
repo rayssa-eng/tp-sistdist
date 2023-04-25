823385364edba93c268eaa6cb9a221a4fe66c2a3
Nick Correa <nicolicorrea@poli.ufrj.br>
2023-04-24 21:25:02 -0300

    teste com pipe

--- TP1/teste.c
+++ TP1/teste.c
@@ -1,123 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
 #include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#define ERRO               1
+int N = 1;
+int i = 0;
+pid_t pid;
+int pipeend[2];
+int mypipe;
+int size = 20;
 
-void signal_handler(int signal) {
-
-    switch (signal) {
-        case SIGUSR1:
-            printf("Sinal SIGUSR1 recebido\n");
-            break;
-        case SIGUSR2:
-            printf("Sinal SIGUSR2 recebido\n");
-            break;
-        case SIGQUIT:
-            printf("Sinal SIGQUIT recebido, logo o processo irá terminar\n");
-            exit(0);
-            break;
-    }
-}
-
-
-int enviaSinal(pid_t pidDestino, int signal){
-
-    printf("Enviando sinal\n");
-
-    if (getpgid(pidDestino) >= 0) {
-        printf("Processo existe\n");
-        kill(pidDestino, signal);
-        printf("Sinal enviado\n");
-        sleep(2);
-        return 0;
-    
-    }else{
-        printf("Processo não existe\n");
-
-        return 1;
-    }
-}
-
-int recebeSinal(int tipoWait){
-
-    printf("Recebendo sinal\n");
-
-    pid_t pid;
-    pid = getpid(); //getpid retorna pid do processo chamado
-    printf("Pid é %d \n",pid); //exibindo pid
-
-    signal(SIGUSR1, signal_handler);
-    signal(SIGUSR2, signal_handler);
-    signal(SIGQUIT, signal_handler);
-
-    if (tipoWait == 0) {
-        while (1); // busy wait
-    }else {
-        while(1){ 
-            pause(); 
-        } // blocking wait
-    }
-
-    return 0;
-}
-
-int main(){
-    
-    int opcao;
-
-    printf("Esse programa recebe e envia sinais!\n");
-
-    printf("Você gostaria de receber ou enviar sinais?\n");
-
-    printf("1 - Receber\n2 - Enviar\n");
-
-    scanf("%d", &opcao);
-    switch (opcao)
+bool isPrime(int n)
+{
+    bool a = true;
+    if (n == 0 || n == 1)
     {
-    case 1:;
-
-        int tipoWait;
-
-        printf("Digite 0 para busy wait ou 1 para blocking wait:\n");
-
-        scanf("%d", &tipoWait);
-
-        recebeSinal(tipoWait);
-
-        break;
-
-    case 2:;
-     
-        int pidDestino;
-        int sinal;
-
-        printf("Qual o pid de Destino?\n");
-        scanf("%d", &pidDestino);
-
-        printf("Digite o número do  sinal desejado:\n");
-        printf("10 - SIGUSR1; 12 - SIGUSR2; 3 - SIGQUIT (Finaliza o programa)\n");
-        scanf("%d", &sinal);
-
-        
-        enviaSinal(pidDestino,sinal);
-        break;    
-    default:
-
-        break;
+        a = false;
     }
+    else
+    {
+        for (int i = 2; i <= n / 2; ++i)
+        {
+            if (n % i == 0)
+            {
+                a = false;
+                break;
+            }
+        }
+    }
+    return a;
+}
 
-    ///Decidir se recebe sinal ou envia 
-    
-    /*pid_t pidDestino;
-    pidDestino = 101513;
-    
-    enviaSinal(pidDestino, 15);*/
-    
-    return 0;
+int produtor(int max)
+{
+    while (i < max + 1)
+    {
+        int delta = rand() % 100;
+        N = N + delta;
+        char w = N + '0';
+        //printf("%d num1 \n e char %c", N,w);
 
+        write(pipeend[1], &w, size);
+        i++;
+
+    }
+    N = 0;
+    char w = N + '0';
+    write(pipeend[1], &w, size);
+    printf("mandou zero");
+}
+
+int j = 0;
+int consumidor()
+{
+    char in;
+    read(pipeend[0], in, size);
+    int r = in - '0';
+    printf(in);
+    read(pipeend[0], in, size);
+    r = in - '0';
+    printf(in);
+    //while (r != 0)
+    //{
+        //printf("%d atual \n", r);
+        //if (isPrime(r))
+        //{
+        //    printf("%d é primo \n", r);
+        //}
+        //read(pipeend[0], in, size);
+    //}
+    printf("terminou");
+}
+
+int main()
+{
+    srand(time(NULL));
+    pid = fork();
+    //printf("meu pid é %d\n", pid);
+    int max = rand() % 1000;
+    mypipe = pipe(pipeend);
+    if (pid < 0)
+    {
+        printf("Erro ao criar novo processo\n");
+    }
+    else if (pid > 0)
+    {
+        //printf("processo 1\n");
+        if (mypipe < 0)
+        {
+            printf("Erro ao criar pipe\n");
+        }
+
+        produtor(max);
+    }
+    else
+    {
+        //printf("processo 2\n");
+        consumidor();
+    }
 }
