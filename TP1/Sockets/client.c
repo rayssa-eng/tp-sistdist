f9d0652d47817c3802395e5b286c2402f362b3d5
Rayssa de Almeida <rayssac.almeida97@gmail.com>
2023-04-27 11:33:25 -0300

    Construindo o cliente

--- TP1/Sockets/client.c
+++ TP1/Sockets/client.c
@@ -20,4 +20,43 @@
 #define MAXLINE 4096 //buffer length where data goes
 #define SA struct sockaddr
 
+void kill_on_error(const char *fmt, ...) {
+    int errno_save;
+    va_list ap;
 
+    //salvando errno pois qualquer chamada (para biblioteca ou sistema operacional) pode atribuir à essa var
+    errno_save = errno;
+
+    //print de fmt+args pra stdout
+    va_start(ap, fmt);
+    vfprintf(stdout,fmt,ap);
+    fprintf(stdout, "\n");
+    fflush(stdout);
+
+   //print mensagem de erro de acordo com o valor setado em err_no primeiramente
+   if (errno_save != 0) {
+       fprintf(stdout, "errno = %d : %s\n", errno_save, strerror(errno_save));
+       fprintf(stdout, "\n");
+       fflush(stdout);
+   }
+   va_end(ap);
+
+   //aqui a gente mata
+   exit(1);
+}
+
+int main(int argc, char **argv) {
+    int sockfd, n;
+    int sendbytes;
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE];
+    char recvline[MAXLINE];
+
+    if (argc != 2) {
+        kill_on_error("Comando: %s <server address>", argv[0]);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        kill_on_error("Erro ao criar o socket!");
+    }
+}
