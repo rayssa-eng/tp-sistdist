576250c5409d4496fcd962fc5caf3db580034c8d
Nick Correa <nicolicorrea@poli.ufrj.br>
2023-07-18 17:22:33 -0300

    mudanças no envio de release

--- TP3/client.c
+++ TP3/client.c
@@ -41,8 +41,6 @@ int main() {
     strcat(grant_id, id);
     strcat(release_id, id);
     
-    //printf("id: %s, request: %s, grant: %s, release: %s \n",id,request_id,grant_id,release_id);
-
     socklen_t len = sizeof(servaddr);
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket creation failed");
@@ -65,7 +63,6 @@ int main() {
 }
 
 int get_access() {
-    //printf("getacess \n");
     char buffer[MAXLINE];
     const char *request = "1";
     const char *grant = "2";
@@ -75,12 +72,11 @@ int get_access() {
     strcpy(grant_id, grant);
     strcat(request_id, id);
     strcat(grant_id, id);
-    //printf("premsg \n");
-    //send(sockfd, request_id, strlen(request_id), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
     send(sockfd, request_id, strlen(request_id), 0);
+    printf(" \n Mensagem enviada request: %s\n", request_id);
+
     socklen_t len = sizeof(servaddr); // Correção adicionada aqui
     int msg;
-    //printf("prerecv \n");
     //msg = recvfrom(sockfd, buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
     msg = recv(sockfd, buffer,MAX_BUFFER_SIZE,0);
     buffer[msg] = '\0';
@@ -95,13 +91,15 @@ int get_access() {
 void my_run() {
     int i = 0;
     FILE *myFile;
-    char release_id[SIZE]; // Declaração adicionada aqui
-    strcpy(release_id, "3");
-    strcat(release_id, id);   
+    myId = getpid();
+    sprintf(id, "%d", myId);
+    const char *release = "3";
+    char release_id[SIZE];
+    strcpy(release_id, release);
+    strcat(release_id, id);
 
     socklen_t len = sizeof(servaddr);
     while (i < r) {
-        //printf("i = %d \n", i);
         int access = get_access();
         if (access == 1) {
             myFile = fopen("resultado.txt", "a");
@@ -121,9 +119,9 @@ void my_run() {
             fclose(myFile);
 
             sleep(k);
-            sendto(sockfd, release_id, strlen(release_id), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+            send(sockfd, release_id, strlen(release_id), 0);
+            printf("\n Mensagem enviada release: %s\n", release_id);
             i++;
-            //printf("i = %d, r = %d \n", i,r);
         }
     }
 }
